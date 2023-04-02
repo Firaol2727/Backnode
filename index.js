8dd49cb8d226dbe091f1252f32c69a087d7bfef1
Firaol Getachew <fraolgetachew2727@gmail.com>
2023-04-02 21:41:43 +0300

    another cor

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ app.use(express.urlencoded({
     extended:true
 }));
 app.use(cors({
-    origin: ['https://harena.onrender.com','http://localhost:7494'],
+    origin: ['https://harena.onrender.com','http://localhost:7494',"https://harena.netlify.app"],
     credentials:true,
 }));
 app.options('*', cors());
