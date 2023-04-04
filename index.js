7514528ccacea6103187f9d1736d382778c89719
Firaol Getachew <fraolgetachew2727@gmail.com>
2023-04-04 11:25:10 +0300

    ade

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ app.use('/custom',customerRoutes);
 app.use('/special',adminRoutes);
 app.use('/sel',sellerRoutes);
 app.use(function (req,res,next){
-    res.header("Access-Control-Allow-Origin","*")
+    res.header("Access-Control-Allow-Origin","https://harena.onrender.com")
 
     res.header("Access-Control-Allow-Methods",'GET,PUT,POST,DELETE,PATCH,OPTIONS')
 
