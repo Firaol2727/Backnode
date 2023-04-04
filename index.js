c3aff9903e3b0a334580b4bd1cd555a79162d2aa
Firaol Getachew <fraolgetachew2727@gmail.com>
2023-04-04 11:16:34 +0300

    Update index.js

--- index.js
+++ index.js
@@ -75,7 +75,7 @@ app.use(function (req,res,next){
 
     res.header("Access-Control-Allow-Methods",'GET,PUT,POST,DELETE,PATCH,OPTIONS')
 
-    res.header("Access-Control-Allow-Credentials",true)
+    res.header("Access-Control-Allow-Headers","Origin, X-Requested-With,Content-Type,Accept,Authorization")
     
 } )
 app.get('/images/:picid',(req,res)=>{
