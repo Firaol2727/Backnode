33ddbf64d78a0761a27546ea4158f7d7dcc643c8
Firaol Getachew <fraolgetachew2727@gmail.com>
2023-04-04 11:16:56 +0300

    another

--- index.js
+++ index.js
@@ -76,7 +76,7 @@ app.use(function (req,res,next){
     res.header("Access-Control-Allow-Methods",'GET,PUT,POST,DELETE,PATCH,OPTIONS')
 
     res.header("Access-Control-Allow-Headers","Origin, X-Requested-With,Content-Type,Accept,Authorization")
-    
+    next()
 } )
 app.get('/images/:picid',(req,res)=>{
     let id=req.params.picid;
